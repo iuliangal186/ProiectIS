2b786b5fb93b4155f8743117039680586a0d620a
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2021-12-06 21:14:00 +0200

    Added listener; Working example

--- mqtt_listener.py
+++ mqtt_listener.py
@@ -26,16 +26,20 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
-def publish(client):
-     msg_count = 0
-     while True:
-         time.sleep(1)
-         msg = f"messages: {msg_count}"
-         result = client.publish(topic, msg)
-         # result: [0, 1]
-         status = result[0]
-         if status == 0:
-             print(f"Send `{msg}` to topic `{topic}`")
-         else:
-             print(f"Failed to send message to topic {topic}")
-         msg_count += 1
+def subscribe(client):
+    def on_message(client,userdata,msg):
+            print(f"Received {msg.payload.decode()} from {msg.topic} topic")
+    client.subscribe(topic)
+    client.on_message=on_message
+
+
+def run():
+    client=connect_mqtt()
+    
+    subscribe(client)
+
+    client.loop_forever()
+    
+
+if __name__=='__main__':
+    run()
