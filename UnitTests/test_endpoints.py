d7394caf09d710b80350fbae52b938f1ce615751
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-01-30 15:02:41 +0200

    Added multiple snesor tests

--- UnitTests/test_endpoints.py
+++ UnitTests/test_endpoints.py
@@ -1,4 +1,5 @@
 import os
+import random
 from tempfile import tempdir
 import tempfile
 import pytest
@@ -11,9 +12,9 @@ import db
 
 # Numai o echipa de idioti a putut crea un asemenea sistem. Ore pierdute incercand sa testez o pagina banala.
 # Un destept ca sa rezolve problema si-a transformat intreaga aplicatie main.py intr-un test.... adica test_main.py
-# Ce o sa faci atunci cand vei crea sa testezi si mqtt-ul?
+# Ce o sa faci atunci cand vei vrea sa testezi si mqtt-ul?
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def client():
     db_fd, db_path = tempfile.mkstemp()
     import server_http
@@ -40,6 +41,35 @@ def test_root_endpoint(client):
 
 def test_sensor_temperature(client):
     landing = client.get("/temperatura/")
+    data = json.loads(landing.data.decode())
+
+    assert "Sensor succesfully read" in data['status']
+    assert data['data']['id']!=0
+    assert data['data']['value']>0 and data['data']['value']<100
+    assert landing.status_code == 200
+
+    # Get a random sample between 0 hours and 24 hours
+    landing = client.get("/temperatura/statistics?time_period="+str(random.randint(0,24)))
+    data = json.loads(landing.data.decode())
+
+    assert "Data succesfully retrieved" in data['status']
+    assert data['data']['average']>0 and data['data']['average']<100
+    assert data['data']['average']==sum(data['data']['history'])/len(data['data']['history'])
+    assert landing.status_code == 200
+
+def test_sensor_light(client):
+    landing = client.get("/lumina/")
+    data = json.loads(landing.data.decode())
+
+    print(landing)
+
+    assert "Sensor succesfully read" in data['status']
+    assert data['data']['id']!=0
+    assert data['data']['value']>0 and data['data']['value']<100
+    assert landing.status_code == 200
+
+def test_sensor_umiditate(client):
+    landing = client.get("/umiditate/")
     data = json.loads(landing.data.decode())
 
     print(landing)
