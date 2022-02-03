2ba4421218e88167cc093d2931edd4fc950390da
sorinNgit <sorinnsg@gmail.com>
2022-02-03 03:15:42 +0200

    Tweak OpenAPI.yaml

--- restler_working_dir/Compile/grammar.py
+++ restler_working_dir/Compile/grammar.py
@@ -80,12 +80,12 @@ requestId="/usa"
 )
 req_collection.add_request(request)
 
-# Endpoint: temperatura, method: Get
+# Endpoint: garbage, method: Get
 request = requests.Request([
     primitives.restler_static_string("GET "),
     primitives.restler_basepath("/"),
     primitives.restler_static_string("/"),
-    primitives.restler_static_string("temperatura"),
+    primitives.restler_static_string("garbage"),
     primitives.restler_static_string(" HTTP/1.1\r\n"),
     primitives.restler_static_string("Accept: application/json\r\n"),
     primitives.restler_static_string("Host: localhost:42178\r\n"),
@@ -93,7 +93,7 @@ request = requests.Request([
     primitives.restler_static_string("\r\n"),
 
 ],
-requestId="temperatura"
+requestId="garbage"
 )
 req_collection.add_request(request)
 
